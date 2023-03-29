336a2b55f70ba36471b5bbcfec73b6822bc2b442
Desiree Moeletsi (SGT) <Desiree.Moeletsi@sanlam.co.za>
2023-03-29 13:18:25 +0200

    Minor styling changes

--- src/components/Form.js
+++ src/components/Form.js
@@ -10,10 +10,10 @@ export class Form extends React.Component
             <div>
                 <form action="" >
                     <fieldset>
-                        <input type="text" name="username" id="username" placeholder="Username" required/>
+                        <input type="text" name="username" id="username" pattern="[a-z0-5]{2,}" placeholder="Username" required/>
                     </fieldset>
                     <fieldset>
-                        <input type="password" name="password" id="password" placeholder = "Password" required />
+                        <input type="password" name="password" id="password" placeholder = "Password" pattern="[a-z0-5]{6,}" required />
                     </fieldset>
                     <input type="submit" className="bttn"/>
                 </form>
