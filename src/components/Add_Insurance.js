51812941546a6d769d1f79527bcee7e502324293
Desiree Moeletsi (SGT) <Desiree.Moeletsi@sanlam.co.za>
2023-04-20 22:21:01 +0200

    Removed unnecessary imports

--- src/components/Add_Insurance.js
+++ src/components/Add_Insurance.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Link , useParams, useNavigate} from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import "../styles/addInsurance.css";
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { Link , useParams, useNavigate} from "react-router-dom";
+import { useNavigate} from "react-router-dom";
 import { toast } from "react-toastify";
 import "../styles/form.css";
 
