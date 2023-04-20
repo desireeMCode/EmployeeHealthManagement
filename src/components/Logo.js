a8c14d6136c549990305b8ef0bd2e9662c61e1b9
Desiree Moeletsi (SGT) <Desiree.Moeletsi@sanlam.co.za>
2023-04-20 15:36:16 +0200

    Fixed conflict

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -6,19 +6,11 @@ export class Logo extends React.Component
     render()
     {
         return(
-<<<<<<< HEAD
-            <div className ="logo">
-                <div className = "ring"></div>
-                <div className = "ring"></div>
-                <div className = "ring"></div>
-                <h1 className ="logoName">EmpHealth</h1>
-=======
             <div className = "logo">
                 <div className = "ring"></div>
                 <div className = "ring"></div>
                 <div className = "ring"></div>
                 <h1 className="logoName">EmpHealth</h1>
->>>>>>> d3115a31491beaec48c039dbd36a101af9834f1f
             </div>
         )
 
